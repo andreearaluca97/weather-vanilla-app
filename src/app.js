36bb891c989eabf8aca1938fae66b567500e1384
Andreea <>
2021-05-12 00:16:41 +0200

    Added the keys from the object

--- src/app.js
+++ src/app.js
@@ -2,6 +2,14 @@ function displayTemperature(response) {
   console.log(response.data.main.temp);
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+  let descriptionElement = document.querySelector("#weather-description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = response.data.wind.speed;
 }
 
 let apiKey = "d4344c6ea95063be2031ed54ff742e0b";
