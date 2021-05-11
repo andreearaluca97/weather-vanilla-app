c99166d0be08eb327aa4543f6fb233349cc0e6bd
Andreea <>
2021-05-12 01:55:44 +0200

    Cleaned Js

--- src/app.js
+++ src/app.js
@@ -17,14 +17,19 @@ function formatDate(timestamp) {
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
+
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = response.data.name;
+
   let descriptionElement = document.querySelector("#weather-description");
   descriptionElement.innerHTML = response.data.weather[0].description;
+
   let humidityElement = document.querySelector("#humidity");
   humidityElement.innerHTML = response.data.main.humidity;
+
   let windElement = document.querySelector("#wind");
   windElement.innerHTML = Math.round(response.data.wind.speed);
+
   let dateElement = document.querySelector("#date");
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
